05c8562de1480b8a7311243696a86a14d38039a0
Armael <armael@isomorphis.me>
2015-12-21 17:35:04 +0100

    Actually substitute %%VERSION%% in the META file

--- pkg/build.ml
+++ pkg/build.ml
@@ -1,8 +1,10 @@
 #!/usr/bin/env ocaml
 #directory "pkg";;
 #use "topkg.ml";;
+#use "config.ml";;
 
 let () =
+  Vars.subst ~skip:Config.subst_skip ~vars:Config.vars ~dir:"." >>& fun () ->
   Pkg.describe "libudev" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/libudev";
